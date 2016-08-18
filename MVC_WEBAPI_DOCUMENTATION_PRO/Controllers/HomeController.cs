d2e3d3906a6193868c383091f858af4e9e0ff49b
Jth Rdz <fernetsystem@gmail.com>
2016-08-17 22:57:06 -0500

    Controlador principla

---
+++ MVC_WEBAPI_DOCUMENTATION_PRO/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_WEBAPI_DOCUMENTATION_PRO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
